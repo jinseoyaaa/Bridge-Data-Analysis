78be0c6107273adcf758370e95482d5b925cf74d
Han JinSeo <jinseo0278@gmail.com>
2023-08-08 15:39:49 +0900

    First Commit

---
+++ code/seoul_safty_class_ratio_map.R
@@ -0,0 +1,80 @@
+install.packages("ggmap")
+install.packages("ggplot2")
+install.packages("raster")
+install.packages("rgeos")
+install.packages("maptools")
+install.packages("rgdal")
+install.packages("sp")
+install.packages("plotly")
+install.packages("sf")
+install.packages("terra")
+install.packages("broom")
+install.packages("ggalt")
+install.packages("ggthemes")
+install.packages("viridis")
+
+library(ggmap)
+library(ggplot2)
+library(raster)
+library(rgeos)
+library(maptools)
+library(rgdal)
+library(sp)
+library(plotly)
+library(sf)
+library(terra)
+library(broom)
+library(ggalt) # devtools::install_github("hrbrmstr/ggalt")
+library(ggthemes)
+library(viridis)
+
+P <- read.csv("data/s_r_mean.csv", header = TRUE) # 시각화할 데이터셋
+map <- shapefile("data/TL_SCCO_SIG.shp") # 지리 정보 데이터셋
+head(map)
+
+# 좌표계 변환
+from_crs <- CRS("+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=1000000 +y_0=2000000 +ellps=GRS80 +units=m")
+to_crs <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+
+map <- spTransform(map, to_crs)
+summary(map)
+head(map)
+
+map <- gBuffer(map, byid=TRUE, width=0)
+new_map <- fortify(map, region="SIG_CD")
+str(new_map)
+head(new_map)
+
+# 서울만 추출
+new_map$id <- as.numeric(new_map$id)
+seoul_map <- new_map[new_map$id <= 11740,]
+
+# merge
+P_merge <- merge(seoul_map, P, by='id')
+
+# 구별 경계
+ggplot() + geom_polygon(data = P_merge, aes(x=long, y=lat, group=group), fill = 'white', color='black')
+
+# 옵션 변경
+ggplot() + geom_polygon(data = P_merge, aes(x=long, y=lat, group=group, fill = mean_safty))
+
+# 이름 넣기
+seoul_district<- read.csv('data/seoul_long_lat.csv', header = T)
+
+# ggplot을 활용해 지도 변형
+plot <- ggplot() + geom_polygon(data = P_merge, aes(x=long, y=lat, group=group, fill = mean_safty))
+plot + scale_fill_gradient(low = "#ffe5e5", high = "#ff3232", space = "Lab", guide = "colourbar") + theme_bw() + labs(title = "서울시 평균 안전등급 분포") + theme(panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank(), panel.grid.major.y = element_blank(), panel.grid.minor.y = element_blank(), plot.title = element_text(face = "bold", size = 18, hjust = 0.5))+geom_text(data = seoul_district, aes(x = long, y = lat, label= paste(시군구명)))
+
+# ggplot을 활용해 지도 변형
+plot <- ggplot() + geom_polygon(data = P_merge, aes(x=long, y=lat, group=group, fill = mean_after))
+plot + scale_fill_gradient(low = "#dce4f0", high = "#0067a3", space = "Lab", guide = "colourbar") + theme_bw() + labs(title = "서울시 구별 준공 후 경과연수") + theme(panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank(), panel.grid.major.y = element_blank(), panel.grid.minor.y = element_blank(), plot.title = element_text(face = "bold", size = 18, hjust = 0.5))+geom_text(data = seoul_district, aes(x = long, y = lat, label= paste(시군구명)))
+
+P_cde <-read.csv("data/s_r_pct_cde.csv", header = TRUE)
+
+P_merge_pct <- merge(seoul_map, P_cde, by='id')
+
+ggplot() + geom_polygon(data = P_merge_pct, aes(x=long, y=lat, group=group), fill = 'white', color='black')
+
+# ggplot을 활용해 지도 변형
+plot <- ggplot() + geom_polygon(data = P_merge_pct, aes(x=long, y=lat, group=group, fill = pct))
+plot + scale_fill_gradient(low = "#dce4f0", high = "#0067a3", space = "Lab", guide = "colourbar") + theme_bw() + labs(title = "서울시 구별 cde교량 비율") + theme(panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank(), panel.grid.major.y = element_blank(), panel.grid.minor.y = element_blank(), plot.title = element_text(face = "bold", size = 18, hjust = 0.5))+geom_text(data = seoul_district, aes(x = long, y = lat, label= paste(시군구명)))
